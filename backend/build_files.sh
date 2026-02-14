2103cb58ff781abc1a28c24578961b0c17a3349b
ahmad <ahmadraza001912@gmail.com>
2026-02-14 23:30:16 +0530

    added the build sh file and vercel.json

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,2 +1,24 @@
-pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput
+#!/bin/sh
+
+echo "Building the project..."
+# Ensure pip is installed
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install --upgrade pip
+
+# Install dependencies
+python3.9 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+
+echo "Collect Static..."
+# Create the output directory if it doesn't exist
+mkdir -p staticfiles_build
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
+
+
+mv static/* staticfiles_build/
